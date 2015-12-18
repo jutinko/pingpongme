b579113140e666bb463553a1783ab3a17b4620e3
Zhou Yu <zyu@pivotal.io>
2015-12-18 15:09:35 +0000

    Implement server fuctionalities
    Signed-off-by: Houssem El Fekih <hfekih@pivotal.io>

---
+++ server.js
@@ -0,0 +1,57 @@
+
+
+var main_view = "You got matched with";
+
+// caveat: default location of London might cause problems
+
+var locations = {}
+
+var interval
+var threshold = 30 * 1000
+
+
+
+
+function cleanupLocations(){
+  for (elem in locations) {
+    if (( Date.now() - locations[elem].timestamp ) > threshold){
+      locations[elem].lastResponse.send("Your ping pong request has timed out.. ");
+      console.log("timeout");
+      locations[elem] = {}
+    }
+  }
+}
+
+exports.requestpair = function(req, res) {
+    var location_to_check = (req.query.location || "London");
+
+    var location_state = update_location_map(location_to_check);
+
+    if (!Boolean(location_state.lastResponse)) {
+      location_state.lastResponse = res;
+      location_state.lastRequest = req;
+      location_state.timestamp = Date.now();
+      return
+    } else {
+      var common_response = main_view + (location_state.lastRequest.params.name || "Unknown coward") + " vs. " + (req.params.name || "Unknown coward at ") + location_to_check;
+
+      location_state.lastResponse.send(common_response);
+      res.send(common_response);
+
+      locations[location_to_check] = {}
+
+      return
+    }
+
+    function update_location_map (location) {
+      if (!Boolean(locations[location])) {
+        locations[location] = {}
+      }
+      return locations[location];
+    }
+}
+
+
+interval = setInterval(cleanupLocations, 1000);
+
+cleanupLocations();
